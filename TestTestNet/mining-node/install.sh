c01c1a87aa72f69e985d322bbf4fa55ad7903363
Alexey Elizarov <felex-ae@ya.ru>
2017-09-20 18:07:04 +0300

    update path to genesis for mining-node

--- TestTestNet/mining-node/install.sh
+++ TestTestNet/mining-node/install.sh
@@ -77,7 +77,7 @@ printenv
 #INSTALL_DOCKER_VERSION="17.03.1~ce-0~ubuntu-xenial"
 #INSTALL_DOCKER_IMAGE="parity/parity:v1.7.2"
 INSTALL_CONFIG_REPO="https://raw.githubusercontent.com/musereum/test-templates/dev/TestTestNet/mining-node"
-GENESIS_REPO_LOC="https://raw.githubusercontent.com/musereum/musereum-scripts/master/spec.json"
+GENESIS_REPO_LOC="https://raw.githubusercontent.com/musereum/musereum-scripts/devtestnet/spec.json"
 GENESIS_JSON="spec.json"
 NODE_TOML="node.toml"
 NODE_PWD="node.pwd"
